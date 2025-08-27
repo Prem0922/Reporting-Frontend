246f986cbf58a33cac2ab76edec815db9fcc1438
Prem0922 <prem22@hydaibotix.com>
2025-08-27 17:03:33 +0530

    Update LoginForm.js

--- test-dashboard-ui/src/components/auth/LoginForm.js
+++ test-dashboard-ui/src/components/auth/LoginForm.js
@@ -118,7 +118,7 @@ const LoginForm = ({ onForgotPasswordClick }) => {
       return;
     }
     try {
-      const res = await fetch('http://localhost:5000/api/login', {
+      const res = await fetch('https://reporting-backend-kpoz.onrender.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
